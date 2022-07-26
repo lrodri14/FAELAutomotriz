48b9f70bb6c0c3b2b419f4db77819bab53476326
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2022-07-26 15:53:45 -0600

    Establecer Descripcion de cada uno de los vehiculos en sus respectivas divisiones de marcas

--- frontend/marcas/vista-marca.js
+++ frontend/marcas/vista-marca.js
@@ -51,6 +51,170 @@ const contenidoModal = {
             },
             precio: '$ 320,000'
         }
+    },
+    lamborghini: {
+        aventador: {
+            titulo: 'Aventador',
+            video: '../../static/video/presentacion/lamborghini/aventador.mp4',
+            logo: '../../static/imgs/marcas/presentacion/lamborghini/lambo-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
+        huracan: {
+            titulo: 'Huracan',
+            video: '../../static/video/presentacion/lamborghini/huracan.mp4',
+            logo: '../../static/imgs/marcas/presentacion/lamborghini/lambo-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
+        urus: {
+            titulo: 'Urus',
+            video: '../../static/video/presentacion/lamborghini/urus.mp4',
+            logo: '../../static/imgs/marcas/presentacion/lamborghini/lambo-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
+    },
+    mclaren: {
+        p1: {
+            titulo: 'P1',
+            video: '../../static/video/presentacion/mclaren/p1.mp4',
+            logo: '../../static/imgs/marcas/presentacion/mclaren/mclaren-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
+        720: {
+            titulo: '720s',
+            video: '../../static/video/presentacion/mclaren/720.mp4',
+            logo: '../../static/imgs/marcas/presentacion/mclaren/mclaren-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
+        600: {
+            titulo: '600LT',
+            video: '../../static/video/presentacion/mclaren/600.mp4',
+            logo: '../../static/imgs/marcas/presentacion/mclaren/mclaren-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
+    },
+    bentley: {
+        continental: {
+            titulo: 'Continental',
+            video: '../../static/video/presentacion/bentley/continental.mp4',
+            logo: '../../static/imgs/marcas/presentacion/bentley/bentley-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
+       bentayga: {
+            titulo: 'Bentayga',
+            video: '../../static/video/presentacion/bentley/bentayga.mp4',
+            logo: '../../static/imgs/marcas/presentacion/bentley/bentley-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
+       spur: {
+            titulo: 'Flying Spur',
+            video: '../../static/video/presentacion/bentley/spur.mp4',
+            logo: '../../static/imgs/marcas/presentacion/bentley/bentley-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
+    },
+    rr: {
+        ghost: {
+            titulo: 'Ghost',
+            video: '../../static/video/presentacion/rr/ghost.mp4',
+            logo: '../../static/imgs/marcas/presentacion/rolls-royce/rolls-royce-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
+        wraith: {
+            titulo: 'Wraith',
+            video: '../../static/video/presentacion/rr/wraith.mp4',
+            logo: '../../static/imgs/marcas/presentacion/rolls-royce/rolls-royce-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
+        cullinan: {
+            titulo: 'Cullinan',
+            video: '../../static/video/presentacion/rr/cullinan.mp4',
+            logo: '../../static/imgs/marcas/presentacion/rolls-royce/rolls-royce-logo.png',
+            mensaje1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            mensaje2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at commodi ea et exercitationem fugiat ipsa maiores minima nobis numquam odio quos, reiciendis rem sed tempora ut, velit vero vitae.',
+            specs: {
+                aceleracion: '2.8s 0-100',
+                top: '360KM/H',
+                torque: '700nM'
+            },
+            precio: '$ 320,000'
+        },
     }
 }
 
