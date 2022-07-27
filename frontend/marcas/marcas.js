973d6bb762052e53f012e6d14f89d0f352048d3f
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2022-07-27 15:50:34 -0600

    Actualizacion de texto en cada una de las secciones individuales, remover Lorem Ipsum

--- frontend/marcas/marcas.js
+++ frontend/marcas/marcas.js
@@ -12,31 +12,31 @@ const vehiculos = document.querySelector('.contenido__info__vehiculos')
 const contenido = {
     ferrari : {
         titulo: 'Ferrari',
-        mensaje: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in faucibus sagittis nunc, vestibulum, aliquet viverra leo nunc. Justo, sit libero non in non, id. Massa, id cras nulla viverra. Dui dignissim commodo at elementum, platea rhoncus et.',
+        mensaje: 'Ferrari es una marca italiana de coches deportivos y superdeportivos, con sede en Maranello y fundada por Enzo Ferrari. En 1929 con la Scuderia Ferrari y en 1947 como fábrica de coches deportivos.',
         imagen: '../static/imgs/marcas/horse.jpg',
         link: '../marcas/ferrari/ferrari.html'
     },
     lamborghini : {
         titulo: 'Lamborghini',
-        mensaje: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in faucibus sagittis nunc, vestibulum, aliquet viverra leo nunc. Justo, sit libero non in non, id. Massa, id cras nulla viverra. Dui dignissim commodo at elementum, platea rhoncus et.',
+        mensaje: 'Lamborghini, es una marca italiana de superdeportivos fundada en 1963 por Ferruccio Lamborghini, un fabricante de tractores fascinado por la tauromaquia y los automóviles deportivos.',
         imagen: '../static/imgs/marcas/bull.jpg',
         link: '../marcas/lamborghini/lamborghini.html'
     },
     mclaren : {
         titulo: 'Mclaren',
-        mensaje: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in faucibus sagittis nunc, vestibulum, aliquet viverra leo nunc. Justo, sit libero non in non, id. Massa, id cras nulla viverra. Dui dignissim commodo at elementum, platea rhoncus et.',
+        mensaje: 'McLaren Automotive es un fabricante británico de superdeportivos, con sede en Woking, Gran Bretaña. Actualmente comercializa una gama de deportivos premium muy apreciada entre los fanáticos de las altas prestaciones.',
         imagen: '../static/imgs/marcas/helmet.jpg',
         link: '../marcas/mclaren/mclaren.html'
     },
     bentley : {
         titulo: 'Bentley',
-        mensaje: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in faucibus sagittis nunc, vestibulum, aliquet viverra leo nunc. Justo, sit libero non in non, id. Massa, id cras nulla viverra. Dui dignissim commodo at elementum, platea rhoncus et.',
+        mensaje: 'Bentley Motors Limited es un fabricante de autos de lujo británico, y subsidiaria total de Volkswagen. Su actividad principal es ingeniería, fabricación y distribución de automóviles de lujo vendidos como Bentley.',
         imagen: '../static/imgs/marcas/bentley.jpg',
         link: '../marcas/bentley/bentley.html'
     },
     rr : {
         titulo: 'Rolls Royce',
-        mensaje: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in faucibus sagittis nunc, vestibulum, aliquet viverra leo nunc. Justo, sit libero non in non, id. Massa, id cras nulla viverra. Dui dignissim commodo at elementum, platea rhoncus et.',
+        mensaje: 'Rolls-Royce Motor Cars es una empresa de automóviles de lujo británica, fundada por Henry Royce y Charles Stewart Rolls en 1904, pero que desde 1998 pertenece al Grupo BMW.',
         imagen: '../static/imgs/marcas/ecstacy.jpg',
         link: '../marcas/rolls-royce/rolls-royce.html'
     }
